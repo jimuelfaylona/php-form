bbce2ddb849799e517383b2d3d97c91343cead87
Mark Jimuel R. Faylona <66340397+jimuelfaylona@users.noreply.github.com>
2020-11-19 09:49:24 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 echo '<p>This is PHP!</p>
 
+if(isset($_GET["FirstName"])){//show feedback
+  echo $_GET["FirstName"];
+}else{//show form
+  echo'
+  <form action=''>
+  First Name: <input type="text" name="FirstName" /><br />
+    <input type="submit" />
+  </form
+  ';
+}
+
 ?>
 <p>This is HTML</p>
