42cf7f29273aea574a603fca526a3fc4b5a6669c
Mark Jimuel R. Faylona <66340397+jimuelfaylona@users.noreply.github.com>
2020-11-24 09:49:13 -0800

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,10 @@ if (isset($_GET["FirstName"])){//show feedback
   <form action="">
   <p>First Name: <input type="text" name="FirstName" /></p>
   <p>Last Name: <input type="text" name="LastName" /></p>
-  
   <p>Favorite Color:</p>
   <p><input type="radio" name="FavoriteColor[]" value="red"/> Red</p>
   <p><input type="radio" name="FavoriteColor[]" value="blue"/> Blue</p>
   <p><input type="radio" name="FavoriteColor[]" value="yellow"/> Yellow</p>
-  
   <p>Sundae Toppings:</p>
   <p><input type="radio" name="Toppings[]" value="oreos"/> Oreos</p>
   <p><input type="radio" name="Toppings[]" value="m&m"/> M & M</p>
